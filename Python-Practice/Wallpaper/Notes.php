f31ac5ecf7ce196e4d8cae8c3568329af386a91d
Peng Hou <dustsnow@gmail.com>
2014-04-27 15:35:45 -0400

    Python script for changing desktop wallpaper; working

---
+++ Python-Practice/Wallpaper/Notes.php
@@ -0,0 +1,5 @@
+http://www.bing.com/az/hprichbg/rb/PortlandWillamette_EN-US11969258313_1920x1080.jpg
+
+http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=1397809837851&pid=hp
+
+
---
+++ Python-Practice/Wallpaper/main.py
@@ -0,0 +1,18 @@
+import urllib
+import json
+import os
+webpage = urllib.urlopen("http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=1397809837851&pid=hp");#get the rss feed from bing
+webpage_str = ""
+#save the rss to a string
+for line in webpage:
+	webpage_str += line
+webpage_json = json.loads(webpage_str)#decode rss string
+date = webpage_json['images'][0]['startdate']#get date
+dest = "/home/penghou/Pictures/BingWallpaper/"+date+".jpg"#construct destination string to save the file
+url = "http://www.bing.com"
+url += webpage_json['images'][0]['url']#construct image url
+url = url.replace("1366","1920")#change url to fetch high resolution image
+url = url.replace("768","1080")#change url to fetch high resolution image
+urllib.urlretrieve(url,dest)
+command = "gsettings set org.gnome.desktop.background picture-uri 'file://" + dest +"'"#construct command of changing linux background
+os.system(command)#execute command to change the desktop
